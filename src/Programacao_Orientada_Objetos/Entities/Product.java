6180871a3d2697db84b0a8179d1a5ed709b86fdf
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-03 12:58:54 -0300

    feature: aprendi a por private/public e get/set

--- src/Programacao_Orientada_Objetos/Entities/Product.java
+++ src/Programacao_Orientada_Objetos/Entities/Product.java
@@ -1,9 +1,12 @@
 package Programacao_Orientada_Objetos.Entities;
 
 public class Product {
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product() {
+    }
 
     public Product(String name, double price, int quantity) {
         this.name = name;
@@ -13,6 +16,26 @@ public class Product {
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    private int getQuantity() {
+        return quantity;
     }
 
     public double totalValueInStock() {
