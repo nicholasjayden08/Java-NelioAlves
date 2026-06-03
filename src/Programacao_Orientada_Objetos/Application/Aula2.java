c9db0fd5f462d97cf5f6c121bd0d2df834dbfa63
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-03 11:33:01 -0300

    Initial Commit

--- src/Programacao_Orientada_Objetos/Application/Aula2.java
+++ src/Programacao_Orientada_Objetos/Application/Aula2.java
@@ -9,16 +9,17 @@ public class Aula2 {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
-        Product product = new Product();
+
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
-        product.name = scanner.nextLine();
+        String name = scanner.nextLine();
         System.out.print("Price: ");
-        product.price = scanner.nextDouble();
+        double price = scanner.nextDouble();
         scanner.nextLine();
         System.out.print("Quantity in stock: ");
-        product.quantity = scanner.nextInt();
+        int quantity = scanner.nextInt();
         scanner.nextLine();
+        Product product = new Product(name, price, quantity);
 
 
         System.out.println();
@@ -26,7 +27,7 @@ public class Aula2 {
 
         System.out.println();
         System.out.print("Enter the number of products to be added in stock: ");
-        int quantity = scanner.nextInt();
+        quantity = scanner.nextInt();
         product.addProducts(quantity);
 
         System.out.println();
