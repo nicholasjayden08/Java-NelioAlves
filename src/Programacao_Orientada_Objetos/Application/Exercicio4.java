e3144ed247fac0600e0767b5c80bf365f33c1191
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-05 13:57:20 -0300

    Learning: Exercicio de fixação

---
+++ src/Programacao_Orientada_Objetos/Application/Exercicio4.java
@@ -0,0 +1,40 @@
+package Programacao_Orientada_Objetos.Application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        int n;
+
+        System.out.print("Quantos numeros voce vai digitar: ");
+        n = scanner.nextInt();
+
+
+
+        double[] vect = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite um numero: ");
+            vect[i] = scanner.nextDouble();
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += vect[i];
+        }
+        double average = sum / n;
+
+        System.out.print("VALORES = ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(vect[i] + "  ");
+        }
+        System.out.println();
+        System.out.println("SOMA = " + String.format("%.2f", sum));
+        System.out.println("MEDIA = " + String.format("%.2f", average));
+    }
+}
