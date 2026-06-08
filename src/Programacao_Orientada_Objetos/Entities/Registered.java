3c85515eb110d28d8710deaa5f3cd6c84eb670c0
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-08 16:46:43 -0300

    Learning: Exercicio de fixação 5

---
+++ src/Programacao_Orientada_Objetos/Entities/Registered.java
@@ -0,0 +1,47 @@
+package Programacao_Orientada_Objetos.Entities;
+
+public class Registered {
+    private Integer id;
+    private String name;
+    private Double salary;
+
+    private Registered() {
+    }
+    public Registered(Integer id, String name, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double percentage) {
+        salary += salary * percentage / 100.0;
+    }
+
+    public String toString() {
+        return id + ", " + name + ", " + String.format("%2.f", salary);
+    }
+}
