f0891ff67035d1f669f0391cce2e9e1aaf80eb34
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-08 16:46:16 -0300

    Learning: Exercicio de fixação 5

---
+++ src/Programacao_Orientada_Objetos/Application/Exercicio7.java
@@ -0,0 +1,67 @@
+package Programacao_Orientada_Objetos.Application;
+
+import Programacao_Orientada_Objetos.Entities.Registered;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio7 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Registered> list = new ArrayList<>();
+
+
+        System.out.print("How many employees will be registered? ");
+            int n = sc.nextInt();
+
+            for (int i=1; i<=n; i++) {
+                System.out.println();
+                System.out.println("Employee #" + i + ": ");
+
+                System.out.print("Id: ");
+                int id = sc.nextInt();
+                while (hasId(list, id)) {
+                    System.out.print("Id already taken. Try again: ");
+                    id = sc.nextInt();
+                }
+
+                System.out.print("Name: ");
+                sc.nextLine();
+                String name = sc.nextLine();
+                System.out.print("Salary: ");
+                double salary = sc.nextDouble();
+                list.add(new Registered(id, name, salary));
+            }
+
+
+            System.out.println();
+            System.out.print("Enter the employee id that will have salary increase: ");
+            int id = sc.nextInt();
+            Registered emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+            if (emp == null) {
+                System.out.println("This id does not exist!");
+            }
+            else {
+                System.out.print("Enter the percentage: ");
+                double percentage = sc.nextDouble();
+                emp.increaseSalary(percentage);
+            }
+
+            System.out.println();
+            System.out.println("List of employees:");
+            for (Registered obj : list) {
+                System.out.println(obj);
+            }
+
+            sc.close();
+        }
+
+        public static boolean hasId(List<Registered> list, int id) {
+            Registered emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+            return emp != null;
+        }
+}
