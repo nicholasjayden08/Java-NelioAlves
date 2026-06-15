69bdeaa6465f26fa5da9024a8ccf39c4d966ea55
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-15 11:51:23 -0300

    Learning: Exercicio de fixação 9/resolvido

---
+++ src/Programacao_Orientada_Objetos/Entities/Comment.java
@@ -0,0 +1,20 @@
+package Programacao_Orientada_Objetos.Entities;
+
+public class Comment {
+    private String text;
+
+    public Comment() {
+    }
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
