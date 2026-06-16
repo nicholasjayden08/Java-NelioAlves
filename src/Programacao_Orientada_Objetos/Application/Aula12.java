6d8fd58c9fb337c4bb3d274ac41f92a25b458691
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-16 07:54:54 -0300

    Learning: Aula sobre Herança

---
+++ src/Programacao_Orientada_Objetos/Application/Aula12.java
@@ -0,0 +1,14 @@
+package Programacao_Orientada_Objetos.Application;
+
+import Programacao_Orientada_Objetos.Entities.BusinessAccount;
+
+public class Aula12 {
+    public static void main(String[] args) {
+
+        BusinessAccount account = new BusinessAccount();
+
+        account.getBalance();
+
+        System.out.println(account);
+    }
+}
