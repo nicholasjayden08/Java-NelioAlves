1d1c47cc7a82d2b03758aab172362e6b493fbb0a
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-16 16:40:22 -0300

    Learning: Aula sobre Sobreposição, Sobrescrita, Super() e @Override

---
+++ src/Programacao_Orientada_Objetos/Entities/BusinessAccount1.java
@@ -0,0 +1,36 @@
+package Programacao_Orientada_Objetos.Entities;
+
+public class BusinessAccount1 extends Account2 {
+    private Double loanLimit;
+
+    public BusinessAccount1() {
+        super();
+    }
+
+    public BusinessAccount1(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(Double amount) {
+        if (amount <= loanLimit) {
+            balance += amount - 10.0;
+        }
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        super.withdraw(amount);
+        balance -= 2.0;
+    }
+
+
+}
