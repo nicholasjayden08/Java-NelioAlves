04902d4b6227a9627c64c9121fd32a02a35dac99
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-18 13:35:02 -0300

    Learning: Exercicio de fixação 12

---
+++ src/Programacao_Orientada_Objetos/Entities/ImportedProduct.java
@@ -0,0 +1,36 @@
+package Programacao_Orientada_Objetos.Entities;
+
+public class ImportedProduct extends Product3 {
+    private Double customsFee;
+
+    public ImportedProduct() {
+        super();
+    }
+
+    public ImportedProduct(String name, Double price, Double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    public Double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(Double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    @Override
+    public String priceTag() {
+        return getName()
+                + " $ "
+                + String.format("%.2f", getPrice())
+                + " (Customs fee: $ "
+                + String.format("%.2f", customsFee)
+                + ")";
+    }
+
+    public Double totalPrice() {
+        return getPrice() + customsFee;
+    }
+}
