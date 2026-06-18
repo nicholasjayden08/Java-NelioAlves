2260f9423984d9e5e46c5f0d8492b76d0f53a8f4
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-18 16:08:46 -0300

    Learning: Aula sobre Métodos Abstratos

---
+++ src/Programacao_Orientada_Objetos/Entities/Circle.java
@@ -0,0 +1,29 @@
+package Programacao_Orientada_Objetos.Entities;
+
+import Programacao_Orientada_Objetos.Entities.Enums.Color;
+
+public class Circle extends Shape{
+    private Double radius;
+
+    public Circle() {
+        super();
+    }
+
+    public Circle(Color color, Double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public Double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public Double area() {
+        return Math.PI * radius * radius;
+    }
+}
