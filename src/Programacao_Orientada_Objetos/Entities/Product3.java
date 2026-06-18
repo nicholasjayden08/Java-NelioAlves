43b9b546b2a7677990807b323050420f4db6d76e
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-18 13:35:23 -0300

    Learning: Exercicio de fixação 12

---
+++ src/Programacao_Orientada_Objetos/Entities/Product3.java
@@ -0,0 +1,34 @@
+package Programacao_Orientada_Objetos.Entities;
+
+public class Product3 {
+    private String name;
+    private Double price;
+
+    public Product3() {
+    }
+
+    public Product3(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String priceTag() {
+        return name + " $ " + String.format("%.2f", price);
+    }
+}
