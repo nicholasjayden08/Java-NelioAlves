8096a67c897c3fbaaeed9005c6b55c2aeccd963d
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-19 12:50:51 -0300

    Learning: Aula sobre exceções

---
+++ src/Programacao_Orientada_Objetos/Application/Aula15.java
@@ -0,0 +1,38 @@
+package Programacao_Orientada_Objetos.Application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Aula15 {
+    public static void main(String[] args) {
+
+        method1();
+
+        System.out.println("End of program");
+
+    }
+    public static void method1() {
+        System.out.println("***METHOD1 START***");
+        method2();
+        System.out.println("***METHOD1 END***");
+    }
+    public static void method2() {
+        System.out.println("***METHOD2 START***");
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            String[] vect = scanner.nextLine().split(" ");
+            int position = scanner.nextInt();
+            System.out.println(vect[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position!");
+            e.printStackTrace();
+            scanner.next();
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Input Error!");
+        }
+        System.out.println("***METHOD2 END***");
+    }
+}
