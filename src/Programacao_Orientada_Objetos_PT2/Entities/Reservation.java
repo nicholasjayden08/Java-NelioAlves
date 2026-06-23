1e7e6676e2845b8381995a60af62b197730e530e
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-23 12:31:21 -0300

    Learning: Aula sobre exceções, solução ruim

--- src/Programacao_Orientada_Objetos_PT2/Entities/Reservation.java
+++ src/Programacao_Orientada_Objetos_PT2/Entities/Reservation.java
@@ -5,19 +5,17 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
+
     private Integer roomNumber;
-    private Date checkin;
-    private Date checkout;
+    private Date checkIn;
+    private Date checkOut;
 
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-    public Reservation() {
-    }
-
-    public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
         this.roomNumber = roomNumber;
-        this.checkin = checkin;
-        this.checkout = checkout;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
 
     public Integer getRoomNumber() {
@@ -28,30 +26,30 @@ public class Reservation {
         this.roomNumber = roomNumber;
     }
 
-    public Date getCheckin() {
-        return checkin;
+    public Date getCheckIn() {
+        return checkIn;
     }
 
-    public void setCheckin(Date checkin) {
-        this.checkin = checkin;
-    }
-
-    public Date getCheckout() {
-        return checkout;
-    }
-
-    public void setCheckout(Date checkout) {
-        this.checkout = checkout;
+    public Date getCheckOut() {
+        return checkOut;
     }
 
     public long duration() {
-        long diff = checkout.getTime() - checkin.getTime();
+        long diff = checkOut.getTime() - checkIn.getTime();
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void updateDates(Date checkin, Date checkout) {
-        this.checkin = checkin;
-        this.checkout = checkout;
+    public String updateDates(Date checkIn, Date checkOut) {
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+            return "Reservation dates for update must be future dates";
+        }
+        if (!checkOut.after(checkIn)) {
+            return "Check-out date must be after check-in date";
+        }
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        return null;
     }
 
     @Override
@@ -59,9 +57,9 @@ public class Reservation {
         return "Room "
                 + roomNumber
                 + ", check-in: "
-                + sdf.format(checkin)
+                + sdf.format(checkIn)
                 + ", check-out: "
-                + sdf.format(checkout)
+                + sdf.format(checkOut)
                 + ", "
                 + duration()
                 + " nights";
