dfeaadc5377ba05c418fcb1f9c6850189baa3134
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-26 16:18:30 -0300

    Learning: Aula sobre leitura de arquivos pt 3, aprendendo sobre FileWriter e BufferedWriter

---
+++ src/Programacao_Orientada_Objetos_PT2/Application/Aula19.java
@@ -0,0 +1,24 @@
+package Programacao_Orientada_Objetos_PT2.Application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Aula19 {
+    static void main() {
+
+        String[] lines = new String[]{"Good Morning", "Good afternoon", "Good Night"};
+
+        String path = "c:\\temp\\out.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
