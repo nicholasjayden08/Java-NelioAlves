43e8b8a2fd6b4b2f8ead05d748b4a3df36920d5f
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-26 15:32:23 -0300

    Learning: Aula sobre leitura de arquivos pt 2, aprendendo sobre FileReader e BufferedReader

---
+++ src/Programacao_Orientada_Objetos_PT2/Application/Aula18.java
@@ -0,0 +1,42 @@
+package Programacao_Orientada_Objetos_PT2.Application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Aula18 {
+    public static void main(String[] args) {
+
+        String path = "/Users/studiocreative/Desktop/in.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
