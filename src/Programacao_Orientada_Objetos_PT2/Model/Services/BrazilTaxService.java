eb61f55927b347987599d4201e2fad6a93339158
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-29 13:11:38 -0300

    Learning: Solução do problema utilizando interfaces.

---
+++ src/Programacao_Orientada_Objetos_PT2/Model/Services/BrazilTaxService.java
@@ -0,0 +1,13 @@
+package Programacao_Orientada_Objetos_PT2.Model.Services;
+
+public class BrazilTaxService implements TaxService {
+
+    public double tax(double amount) {
+        if (amount <= 100.0) {
+            return amount * 0.2;
+        }
+        else {
+            return amount * 0.15;
+        }
+    }
+}
