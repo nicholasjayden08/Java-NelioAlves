acdbf206cbd102907d18390c9293747d3bfec67c
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-29 13:14:24 -0300

    Learning: Solução do problema utilizando interfaces.

---
+++ src/Programacao_Orientada_Objetos_PT2/Model/Entities/Vehicle.java
@@ -0,0 +1,20 @@
+package Programacao_Orientada_Objetos_PT2.Model.Entities;
+
+public class Vehicle {
+    private String model;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
