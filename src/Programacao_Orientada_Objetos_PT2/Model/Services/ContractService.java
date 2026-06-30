d2be7518c91f42cb0a6239e66834b7836ff9f7f7
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-06-30 15:06:41 -0300

    Learning: Exercicio de fixação sobre interfaces

---
+++ src/Programacao_Orientada_Objetos_PT2/Model/Services/ContractService.java
@@ -0,0 +1,26 @@
+package Programacao_Orientada_Objetos_PT2.Model.Services;
+
+import Programacao_Orientada_Objetos_PT2.Model.Entities.Contract;
+import Programacao_Orientada_Objetos_PT2.Model.Entities.Installment;
+
+import java.time.LocalDate;
+
+public class ContractService {
+
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract(Contract contract, int months) {
+        double basicQuota = contract.getTotalValue() / months;
+        for (int i = 1; i <= months; i++) {
+            LocalDate dueDate = contract.getDate().plusMonths(i);
+            double interest = onlinePaymentService.interest(basicQuota, i);
+            double fee = onlinePaymentService.paymentFee(basicQuota + interest);
+            double quota = basicQuota + interest + fee;
+            contract.getInstallments().add(new Installment(dueDate, quota));
+        }
+    }
+}
