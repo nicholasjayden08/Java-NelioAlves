cc0693d7105449c44d3e2443a93485d4d93d5cc4
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-07-16 11:57:09 -0300

    Learning: Aula sobre a introdução Curingas Delimitados

---
+++ src/Programacao_Orientada_Objetos_PT2/Model/Entities/Circle.java
@@ -0,0 +1,23 @@
+package Programacao_Orientada_Objetos_PT2.Model.Entities;
+
+public class Circle implements Shape {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
